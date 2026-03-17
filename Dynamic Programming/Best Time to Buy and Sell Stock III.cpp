e18c8f2112c4436791a6ac0df32e10128a26c498
RayRayChen <157879344+Raychen0060@users.noreply.github.com>
2026-03-17 11:15:35 +0800

    Create Best Time to Buy and Sell Stock III.cpp

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int f1=-prices[0];
+        int f2=0;
+        int f3=-prices[0];
+        int f4=0;
+        for(int i=1;i<prices.size();++i){
+            f1=max(f1,-prices[i]);
+            f2=max(f2,f1+prices[i]);
+            f3=max(f3,f2-prices[i]);
+            f4=max(f4,f3+prices[i]);
+        }
+        return f4;
+    }
+};
