bf7592933743e940412ce616297fa0c9e0e435de
RayRayChen <157879344+Raychen0060@users.noreply.github.com>
2026-05-24 15:42:07 +0800

    Update d686._10003 - Cutting Sticks.cpp

--- Dynamic Programming/d686._10003 - Cutting Sticks.cpp
+++ Dynamic Programming/d686._10003 - Cutting Sticks.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(nullptr); // IO 優化
     int L, N, cost[52][52], cut[52];
-    for (int i=0; i<=50; ++i) cost[i][i+1] = 0;
+    for (int i=0; i<=50; ++i) cost[i][i+1] = 0; // 初始化：因為在 i 和 i+1 之間無法切一刀，所以為0
     
     while (cin>>L && L) { 
         cin >> N;
@@ -13,7 +13,7 @@ int main() {
         
         for (int d=2; d<=N+1; ++d){
             for (int i=0; i<=N-d+1; ++i) {
-                cost[i][i+d] = INT_MAX;
+                cost[i][i+d] = INT_MAX; // 
                 for (int j=i+1; j<i+d; ++j) {
                     int temp = cost[i][j] + cost[j][i+d];
                     if (temp < cost[i][i+d]) cost[i][i+d] = temp;
